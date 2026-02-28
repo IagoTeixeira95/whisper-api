9f936263546e889b0fc1f0e572a16f220aaae6b4
Iago Teixeira <iago.pita@icloud.com>
2026-02-28 10:51:11 -0300

    subindo aplicacao

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, UploadFile, File
+import whisper
+import shutil
+
+app = FastAPI()
+model = whisper.load_model("base")
+
+@app.post("/transcribe")
+async def transcribe(file: UploadFile = File(...)):
+    with open("audio.wav", "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    result = model.transcribe("audio.wav", language="pt")
+    return {"text": result["text"]}
